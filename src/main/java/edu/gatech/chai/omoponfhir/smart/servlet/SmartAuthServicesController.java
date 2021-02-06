07f537ded16625e51aaea969c6c7804c2b057d26
Myung Choi <myungchoi@gmail.com>
2021-02-05 23:41:35 -0500

    scope and scope wild card fixed

--- src/main/java/edu/gatech/chai/omoponfhir/smart/servlet/SmartAuthServicesController.java
+++ src/main/java/edu/gatech/chai/omoponfhir/smart/servlet/SmartAuthServicesController.java
@@ -306,7 +306,9 @@ public class SmartAuthServicesController {
 		String[] scopeEntries = scope.split(" ");
 		String myScope = smartApp.getScope();
 		for (String scopeEntry : scopeEntries) {
-			if (!myScope.contains(scopeEntry)) {
+			if (!(myScope.contains("user/*.*") && scopeEntry.startsWith("user/"))
+ 					&& !(myScope.contains("patient/*.*") && scopeEntry.startsWith("patient/"))
+ 					&& !myScope.contains(scopeEntry)) {
 				// Out of scope
 				try {
 					logger.info("scope, " + scopeEntry + ", is not valid");
@@ -951,10 +953,12 @@ public class SmartAuthServicesController {
 		return scope.trim();
 	}
 
-	private String makeScope(String selectedScopes) {
+	private String makeScope(String appType, String selectedScopes) {
 		// Just do the sanity check. And, remove any duplicates.
 		String[] scopes = selectedScopes.split(" ");
-		String scope = "";
+		String scope = "launch profile openid online_access ";
+ 		if ("Patient".equals(appType))
+ 			scope += "launch/patient ";
 
 		for (String scope_ : scopes) {
 			scope_ = scope_.trim().replaceAll("\\s+", " ");
@@ -1026,7 +1030,7 @@ public class SmartAuthServicesController {
 					patient_encounter_w, patient_medicationstatement_w, patient_medicationrequest_w,
 					patient_observation_w, patient_patient_w, patient_procedure_w);
 		} else {
-			scope = makeScope(selectedScopes);
+			scope = makeScope(appType, selectedScopes);
 		}
 
 		SmartOnFhirAppEntry appEntry = new SmartOnFhirAppEntry();
@@ -1121,7 +1125,7 @@ public class SmartAuthServicesController {
 					patient_encounter_w, patient_medicationstatement_w, patient_medicationrequest_w,
 					patient_observation_w, patient_patient_w, patient_procedure_w);
 		} else {
-			scope = makeScope(selectedScopes);
+			scope = makeScope(appType, selectedScopes);
 		}
 
 		SmartOnFhirAppEntry appEntry = smartOnFhirApp.getSmartOnFhirApp(appId);
