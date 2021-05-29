9351639320a6f2b5d06e9511c35bc1f5efb6f5ba
Myung Choi <myungchoi@gmail.com>
2021-05-29 12:20:02 -0400

    Removing old commented codes

--- src/main/java/edu/gatech/chai/omoponfhir/r4/security/SMARTonFHIRConformanceStatement.java
+++ src/main/java/edu/gatech/chai/omoponfhir/r4/security/SMARTonFHIRConformanceStatement.java
@@ -60,22 +60,6 @@ public class SMARTonFHIRConformanceStatement extends ServerCapabilityStatementPr
 	String tokenURIvalue = "http://localhost:8080/token";
 
 	public SMARTonFHIRConformanceStatement(RestfulServer theRestfulServer) {
-//		super(theRestfulServer);
-//		setCache(false);
-
-//		try {
-//			InetAddress addr = java.net.InetAddress.getLocalHost();
-//			System.out.println(addr);
-//			String hostname = addr.getCanonicalHostName();
-//			System.out.println("Hostname of system = " + hostname);
-//
-//			// authorizeURIvalue = "http://"+hostname+":9085/authorize";
-//			// tokenURIvalue = "http://"+hostname+":9085/token";
-//			// registerURIvalue = "http://"+hostname+":9085/register";
-//		} catch (UnknownHostException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 	@Override
@@ -116,31 +100,11 @@ public class SMARTonFHIRConformanceStatement extends ServerCapabilityStatementPr
 			tokenExtension.setUrl(tokenURI);
 			tokenExtension.setValue(new UriType(tokenURIvalue));
 
-//		Extension registerExtension = new Extension();
-//		registerExtension.setUrl(registerURI);
-//		registerExtension.setValue(new UriType(registerURIvalue));
-
 			secExtension.addExtension(authorizeExtension);
 			secExtension.addExtension(tokenExtension);
-//		secExtension.addExtension(registerExtension);
 
 			restSec.addExtension(secExtension);
-
-			// restSec.addUndeclaredExtension(authorizeExtension);
-			// restSec.addUndeclaredExtension(tokenExtension);
-			// restSec.addUndeclaredExtension(registerExtension);
-
 			rest.setSecurity(restSec);
-
-//		List<CapabilityStatementRestComponent> rests = conformanceStatement.getRest();
-//		if (rests == null || rests.size() <= 0) {
-//			CapabilityStatementRestComponent rest = new CapabilityStatementRestComponent();
-//			rest.setSecurity(restSec);
-//			conformanceStatement.addRest(rest);
-//		} else {
-//			CapabilityStatementRestComponent rest = rests.get(0);
-//			rest.setSecurity(restSec);
-//		}
 		}
 		return conformanceStatement;
 	}
