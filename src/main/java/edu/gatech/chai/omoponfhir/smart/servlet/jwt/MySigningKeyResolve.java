90fb669678544dc675577be4ee289892fc370a88
Myung Choi <myungchoi@gmail.com>
2022-02-10 18:01:56 -0500

    removing unused import

--- src/main/java/edu/gatech/chai/omoponfhir/smart/servlet/jwt/MySigningKeyResolve.java
+++ src/main/java/edu/gatech/chai/omoponfhir/smart/servlet/jwt/MySigningKeyResolve.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.gatech.chai.omoponfhir.smart.dao.JwkSetImpl;
 import edu.gatech.chai.omoponfhir.smart.model.JwkSetEntry;
